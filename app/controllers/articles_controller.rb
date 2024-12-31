f8dc01fe2fb805e48333873f056d2836dccdf7e0
RecheEduardo <Eduardorechemartins2006@gmail.com>
2024-12-31 12:20:20 -0300

    feature: adicionando rota de edição de artigos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,6 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
-
   def new # Inicia a criação do novo artigo
     @article = Article.new
   end
@@ -17,6 +13,23 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])  
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
   end
 
