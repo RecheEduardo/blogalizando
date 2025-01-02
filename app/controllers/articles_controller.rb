9a59ddddbbbc4d361b5b2e6ff012d40ba6d4ff94
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-01-02 19:39:50 -0300

    feature: implementando flash messages também no CRUD de articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to @article
+      redirect_to @article, notice: "Article was successfully created."
     else
       render :new
     end
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
   # UPDATE - Executa a edição do Artigo
   def update 
     if @article.update(article_params)
-      redirect_to @article
+      redirect_to @article, notice: "Article was successfully updated."
     else
       render :edit
     end
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
   # DELETE - Executa a ação de excluir o Artigo
   def destroy
     @article.destroy
-    redirect_to root_path
+    redirect_to root_path, notice: "Article was successfully destroyed."
   end
 
   private
