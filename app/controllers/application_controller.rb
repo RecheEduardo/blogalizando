9d5dbb6d19cd24326291118c95a44463a64ea234
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-01-03 22:17:23 -0300

    feature: implementação de alert para barrar ação indevida no article

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  include Pundit::Authorization 
   # permite o uso da gem Pundit no projeto
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back_or_to(root_path)
+  end
 end
