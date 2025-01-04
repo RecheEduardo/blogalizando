78e7f3d0271c9da912bc1f970aeb0795e3a351e1
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-01-04 11:43:42 -0300

    fix: alterando a rota de acesso inicial do site para a homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
