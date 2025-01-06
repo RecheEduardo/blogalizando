5952ef1dafa27a3da0410db4acb4ee0a1277a027
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-01-06 19:43:39 -0300

    fix: removendo desabilitação global do bullet

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,6 @@ module BlogORama
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
-    #config para fazer o factorybot funcionar
-    config.after_initialize do
-      Bullet.enable = false
-    end
-
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
